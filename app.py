bdc54645e50de01491ce6ff9b5ed3b3f008e4c7a
lemon-squeezy <AdrianLaub@aol.com>
2020-12-30 10:56:32 +0000

    Style changes added

--- app.py
+++ app.py
@@ -103,7 +103,6 @@ def profile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-
     if session["user"]:
         return render_template("profile.html", username=username)
 
--- static/js/script.js
+++ static/js/script.js
@@ -6,5 +6,11 @@
     $('select').formSelect();
     $('.modal').modal();
     // $('.datepicker').datepicker();
-  
+    
+    // Back to Top function
+  var offset = 100;
+  var speed = 250; var duration = 500;
+  $(window).scroll(function(){ 
+      if ($(this).scrollTop() < offset) { $('.topbutton') .fadeOut(duration); } else { $('.topbutton') .fadeIn(duration); } }); $('.topbutton').on('click', function(){ $('html, body').animate({scrollTop:0}, speed); 
+      return false; });
   });
