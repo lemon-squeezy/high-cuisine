803193ec97ecae71c2a5ec3f870bfd356b348ef7
lemon-squeezy <AdrianLaub@aol.com>
2021-01-06 22:52:35 +0000

    Favicon and Error Pages added

--- app.py
+++ app.py
@@ -231,6 +231,16 @@ def terms():
     return render_template('terms.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@ app.errorhandler(500)
+def internal_error(error):
+    return render_template('errors/500.html'), 500
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT'
             )), debug=True)
